da645f8411144e824e7955ea0affe9885a603ef4
Su Li <hiiamsuli@gmail.com>
2018-10-05 22:10:34 -0700

    content update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,15 +3,13 @@ import React from 'react'
 const Content = () => (
   <section className='footer'>
     <div className='container'>
-      <span>2018 &#169; Developed with
-        <a href="https://www.gatsbyjs.org/" target="_blank">GatsbyJS</a>
-        by Su Li</span>
+      <span>2018 &#169; Su Li - Front End Developer & UX Designer</span>
       <span>
         <a href="https://dribbble.com/crispipear" target="_blank">DRIBBBLE</a>—
         <a href="https://codepen.io/crispipear/" target="_blank">CODEPEN</a>—
         <a href="https://github.com/crispipear" target="_blank">GITHUB</a>—
-        <a href="https://www.linkedin.com/in/su-li-4b3403130" target="_blank">LINKEDIN</a>—
-        <a href="mailto:hiiamsuli@gmail.com">EMAIL</a>
+        <a href="https://linkedin.com/in/suyingli/" target="_blank">LINKEDIN</a>—
+        <a href="mailto:lisuy97@gmail.com">EMAIL</a>
       </span>
     </div>
   </section>
--- src/components/about/About.js
+++ src/components/about/About.js
@@ -26,13 +26,13 @@ export default class Content extends Component {
                 <div className='img'>
                   <img src={Profile}/>
                 </div>
-                <p><a href="mailto:hiiamsuli@gmail.com">SAY HELLO</a> &#x1f4ac;</p>
+                <p><a href="mailto:lisuy97@gmail.com">lisuy97@gmail.com</a> &#x1f4ac;</p>
                 <p><a href={Resume} target="_blank">SEE RESUME</a> &#x1f31a;</p>
               </div>
               <div className='aboutContent'>
                 <h1>Hi, my name is Su Li / 李素莹</h1>
                 <p>
-                  I am a <span>Front End Developer & UX Designer</span> from the Greater Seattle Area. Focused on building beautiful products and designing user experiences that offers efficient
+                  I am a <span>Front End Developer and UX Designer</span> from the Greater Seattle Area. Focused on building beautiful products and designing user experiences that offers efficient
                   human centered solutions to problems.
                 </p>
                 <p>
@@ -58,10 +58,6 @@ export default class Content extends Component {
                 </div>
                 <p>
                   Originally my primary focus was visual communication, but over the years technology has influenced me to the development path.
-                  My experience of collaborating with engineers and building application supports to deliver consumer experiences was one of the highlights in
-                  my life. It has inspired me to keep learning about design and development. 
-                </p>
-                <p>
                   You can see more of my visual design work on <a href="https://dribbble.com/crispipear" target="_blank">Dribbble</a>. I also like to experiment and break some stuff in the back end.
                   When I am not breaking stuff I like to play games and eat good food :^)
                 </p>
